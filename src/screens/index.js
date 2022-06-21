b3c6586e286f0f6c85f9876dfd14163cbb2ec83a
Ashish <73551845+sonic-sabers@users.noreply.github.com>
2022-06-21 10:56:03 +0530

    Removed extra content

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,9 +1,4 @@
 export { default as Splashscreen } from './Splashscreen';
 export { default as Loginscreen } from './Loginscreen';
-// export { default as Drawercontent } from './Drawercontent';
 export { default as Homescreen } from './Homescreen';
 export { default as Bottomtab } from './Bottomtab';
-// export { default as Course } from './Course';
-// export { default as Course } from './Course';
-// export { default as Course } from './Course';
-// export { default as Course } from './Course';
